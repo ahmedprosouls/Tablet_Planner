8719782f7058220a65f142be8da734e5c29e5f11
Muzammil Dalia <muzammil.mehboob008@gmail.com>
2023-07-14 15:35:12 +0500

    muju com

---
+++ Controllers/taskControllers.js
@@ -0,0 +1,94 @@
+import taskModel from "../Models/taskModel.js";
+import userModel from "../Models/userModel.js";
+import notesModel from "../Models/notesModel.js";
+import fs from 'fs';
+import multer from "multer";
+
+export const createTasksController = async (req, res) => {
+    try {
+        const { title, category, text, paths, recordings, image } = req.body;
+        // console.log(recordings)
+        const userId = req.user._id;
+        if (!title) {
+            return res.status(401).send({
+                message: "Title is required",
+            });
+        }
+        const existingUser = await userModel.findById(userId);
+        if (!existingUser) {
+            return res.status(404).send({
+                success: false,
+                message: "User not found",
+            });
+        }
+        console.log(image)
+        // const recordings = [];
+
+        // // Loop through the recording files
+        // for (const file of req.files) {
+        //     const fileBuffer = fs.readFileSync(file.path);
+        //     const base64Data = fileBuffer.toString("base64");
+
+        //     const recording = {
+        //         duration: file.originalname,
+        //         file: {
+        //             data: fileBuffer,
+        //             contentType: file.mimetype,
+        //         },
+        //     };
+
+        //     recordings.push(recording);
+
+        //     // Delete the temporary file
+        //     fs.unlinkSync(file.path);
+        // }
+        // const imageBuffer = fs.readFileSync(req.file.path);
+        const task = await new taskModel({
+            person: userId,
+            title,
+            category,
+            text,
+            paths,
+            recordings,
+            image,
+        }).save();
+        res.status(201).send({
+            success: true,
+            message: "Task Successfully Saved",
+            task: {
+                _id: task._id,
+                title: task.title,
+                category: task.category,
+                paths: task.paths,
+                recordings: task.recordings,
+                image: task.image,
+                person: {
+                    _id: existingUser._id,
+                    name: existingUser.name,
+                },
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error in creating Tasks",
+            error,
+        });
+    }
+}
+
+export const getTasksController = async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const Tasks = await taskModel.find({ person: userId }).exec();
+        res.status(200).send(Tasks);
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            message: "Error in getting Tasks",
+            error,
+        });
+    }
+}
---
+++ Models/taskModel.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+    {
+        person: {
+            type: mongoose.ObjectId,
+            ref: "users",
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+
+        category: {
+            type: String,
+            required: true,
+        },
+        paths: {
+            type: [[String]],
+        },
+        recordings: {
+            type: [mongoose.Schema.Types.Mixed]
+        },
+        // recordings: [
+        //     {
+        //         duration: {
+        //             type: String,
+        //             required: true,
+        //         },
+        //         file: {
+        //             // data: Buffer,
+        //             // contentType: String,
+        //             type: String,
+        //             required: true,
+        //         },
+        //         sound: {
+        //             type: Object,
+        //             required: true,
+        //         }
+        //     },
+        // ],
+        // images: [
+        //     {
+        //         height: {
+        //             type: Number,
+        //             required: true,
+        //         },
+        //         imageurl: {
+        //             type: String,
+        //             required: true,
+        //         },
+        //         width: {
+        //             type: Number,
+        //             required: true,
+        //         },
+        //     },
+        // ],
+        image: {
+            type: [mongoose.Schema.Types.Mixed]
+        }
+        ,
+        text: {
+            type: String,
+        }
+
+    },
+    { timestamps: true }
+)
+
+export default mongoose.model("tasks", taskSchema);
